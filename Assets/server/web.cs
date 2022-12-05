4c63d474afcb7c102e49d54a89e710be0c028966
coolblue25721853 <coolblue1853@gmail.com>
2022-12-05 23:51:06 +0900

    22.12.05 오후 11:50 / mysql 서버 연동 및 xampp 이용 유저데이터 삽입

---
+++ Assets/server/web.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+
+// UnityWebRequest.Get example
+
+// Access a website and use UnityWebRequest.Get to download a page.
+// Also try to download a non-existing page. Display the error.
+
+public class web : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(getText());
+        
+    }
+
+    IEnumerator getText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost/winterDev_Backend/getData.php"))
+        {
+            yield return www.Send();
+
+            if(www.isNetworkError || www.isHttpError){
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+
+                byte[] result = www.downloadHandler.data;
+            }
+
+        }
+    }
+}
